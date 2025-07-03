60a123fd351c285d1749f32abe0db6f371c1f91e
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-07-03 14:22:03 +0300

    change app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -4,7 +4,9 @@ import taskRoutes from './routes/taskRoutes';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin:'https://task-manager-v10.vercel.app/'
+}));
 app.use(express.json());
 
 app.use('/api/tasks', taskRoutes);
