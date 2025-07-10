8c7d1eb7ee2de5e149a895f6a3dfb453cfdf8470
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-07-10 11:44:14 +0300

    change origin

--- backend/app.ts
+++ backend/app.ts
@@ -4,9 +4,21 @@ import taskRoutes from './routes/taskRoutes';
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://task-manager-v10.vercel.app'
+];
+
 app.use(cors({
-  origin:'https://task-manager-v10.vercel.app'
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
 }));
+
 app.use(express.json());
 
 app.use('/api/tasks', taskRoutes);
